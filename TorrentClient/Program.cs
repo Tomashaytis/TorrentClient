25dc7ba71f9192763283eb262711fb2aed4be8b2
pavel <tomashaytis@gmail.com>
2025-11-02 12:38:42 +0400

    test: протестирован парсинг на тестовом файле.

--- TorrentClient/Program.cs
+++ TorrentClient/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace TorrentClient
+﻿using TorrentClient.Domain.Parser;
+
+namespace TorrentClient;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var stream = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", args[0]));
+
+        var data = new BencodeParser(stream).Parse();
+
+        Console.WriteLine();
     }
 }
