39b5ba29320a621ad7090ad6ab66651453ffc3ab
pavel <tomashaytis@gmail.com>
2025-11-02 04:03:01 +0400

    Добавьте файлы проекта.

---
+++ TorrentClient.Domain/Parser/BencodeParser.cs
@@ -0,0 +1,152 @@
+﻿namespace TorrentClient.Domain.Parser;
+
+public class BencodeParser(Stream stream)
+{
+    public Stream Stream { get; private set; } = stream;
+
+    public object Parse()
+    {
+        var curSymbol = NextSymbol();
+
+        return curSymbol switch
+        {
+            'i' => ParseInteger(),
+            'l' => ParseList(),
+            'd' => ParseDictionary(),
+            '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' => ParseString(curSymbol),
+            _ => throw new FormatException($"Unknown prefix: {curSymbol}"),
+        };
+    }
+
+    public object Parse(char curSymbol)
+    {
+        return curSymbol switch
+        {
+            'i' => ParseInteger(),
+            'l' => ParseList(),
+            'd' => ParseDictionary(),
+            '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' => ParseString(curSymbol),
+            _ => throw new FormatException($"Unknown prefix: {curSymbol}"),
+        };
+    }
+
+    public string ParseString(char startSymbol)
+    {
+        var bytesCountStr = "" + startSymbol;
+
+        char curSymbol;
+        do
+        {
+            curSymbol = NextSymbol();
+
+            if (!"0123456789:".Contains(curSymbol)) {
+                throw new FormatException($"Invalid symbol in byte count value: {curSymbol}");
+            }
+
+            if (curSymbol != ':')
+                bytesCountStr += curSymbol;
+
+        } while (curSymbol != ':');
+        
+        int bytesCount = int.Parse(bytesCountStr);
+
+        var str = "";
+
+        for (int i = 0; i < bytesCount; i++) 
+        {
+            str += NextSymbol();
+        }
+
+        return str;
+    }
+
+    public long ParseInteger()
+    {
+        var integerStr = "";
+
+        bool first = true;
+        char curSymbol;
+        do
+        {
+            var curByte = NextSymbol();
+            curSymbol = curByte;
+
+            if (!"012345678e9".Contains(curSymbol))
+            {
+                if (curSymbol != '-' || !first)
+                    throw new FormatException($"Invalid digit: {curSymbol}");
+            }
+
+            if (curSymbol != 'e')
+                integerStr += curSymbol;
+
+        } while (curSymbol != 'e');
+
+        if (integerStr.Length == 0)
+            throw new FormatException($"Integer value not found");
+
+        long integer = long.Parse(integerStr);
+
+        return integer;
+
+    }
+
+    public List<object> ParseList()
+    {
+        var list = new List<object>();
+        char curSymbol;
+        do
+        {
+            curSymbol = NextSymbol();
+
+            if (curSymbol != 'e')
+                list.Add(Parse(curSymbol));
+        } while (curSymbol != 'e');
+
+        return list;
+    }
+
+    public Dictionary<string, object> ParseDictionary()
+    {
+        var dictionary = new Dictionary<string, object>();
+        string? previousKey = null;
+
+        char curSymbol;
+        do
+        {
+            curSymbol = NextSymbol();
+
+            if (curSymbol != 'e') {
+                var key = Parse(curSymbol);
+                if (key is string stringKey)
+                {
+                    if (stringKey == "")
+                        throw new FormatException($"Empty string is used as key value");
+
+                    curSymbol = NextSymbol();
+                    if (curSymbol == 'e')
+                        throw new FormatException($"Missing value for key '{stringKey}'");
+
+                    if (dictionary.ContainsKey(stringKey))
+                        throw new FormatException($"Key '{stringKey}' already exists in dictionary");
+
+                    if (previousKey != null && string.Compare(previousKey, stringKey, StringComparison.Ordinal) >= 0)
+                        throw new FormatException($"Keys are not sorted: '{previousKey}' should come before '{stringKey}'");
+
+                    dictionary[stringKey] = Parse(curSymbol);
+                }
+            }
+        } while (curSymbol != 'e');
+
+        return dictionary;
+    }
+
+    public char NextSymbol()
+    {
+        var nextByte = Stream.ReadByte();
+        if (nextByte == -1)
+            throw new EndOfStreamException();
+        return (char)nextByte;
+    }
+}
+
---
+++ TorrentClient.Test/BencodeParserTest.cs
@@ -0,0 +1,206 @@
+using System.Text;
+using TorrentClient.Domain.Parser;
+
+namespace TorrentClient.Test;
+
+public class TestBencodeParser
+{
+    [Theory]
+    [MemberData(nameof(GetStringTestData))]
+    public void TestParseString(string bencodeInput, string expected)
+    {
+        var bytes = Encoding.UTF8.GetBytes(bencodeInput);
+        using var stream = new MemoryStream(bytes);
+
+        var parser = new BencodeParser(stream);
+        var actual = parser.Parse();
+
+        Assert.IsType<string>(actual);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetIntegerTestData))]
+    public void TestParseInteger(string bencodeInput, long expected)
+    {
+        var bytes = Encoding.UTF8.GetBytes(bencodeInput);
+        using var stream = new MemoryStream(bytes);
+
+        var parser = new BencodeParser(stream);
+        var actual = parser.Parse();
+
+        Assert.IsType<long>(actual);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetListTestData))]
+    public void TestParseList(string bencodeInput, List<object> expected)
+    {
+        var bytes = Encoding.UTF8.GetBytes(bencodeInput);
+        using var stream = new MemoryStream(bytes);
+
+        var parser = new BencodeParser(stream);
+        var actual = parser.Parse();
+
+        Assert.IsType<List<object>>(actual);
+        var expectedList = expected.ToList();
+        var actualList = (List<object>)actual;
+
+        Assert.Equal(expectedList.Count, actualList.Count);
+
+        AssertBencodeListEqual(expectedList, actualList);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetDictionaryTestData))]
+    public void TestParseDictionary(string bencodeInput, Dictionary<string, object> expected)
+    {
+        var bytes = Encoding.UTF8.GetBytes(bencodeInput);
+        using var stream = new MemoryStream(bytes);
+
+        var parser = new BencodeParser(stream);
+        var actual = parser.Parse();
+
+        Assert.IsType<Dictionary<string, object>>(actual);
+        var expectedDictionary = expected.ToDictionary();
+        var actualDictionary = (Dictionary<string, object>)actual;
+
+        Assert.Equal(expectedDictionary.Count, actualDictionary.Count);
+
+        AssertBencodeDictionaryEqual(expectedDictionary, actualDictionary);
+    }
+
+    private void AssertBencodeListEqual(List<object> expected, List<object> actual)
+    {
+        Assert.Equal(expected.Count, actual.Count);
+
+        for (int i = 0; i < expected.Count; i++)
+        {
+            AssertBencodeEqual(expected[i], actual[i]);
+        }
+    }
+
+    private void AssertBencodeDictionaryEqual(Dictionary<string, object> expected, Dictionary<string, object> actual)
+    {
+        Assert.Equal(expected.Count, actual.Count);
+
+        foreach (var key in expected.Keys)
+        {
+            Assert.True(actual.ContainsKey(key), $"Key '{key}' not found in actual dictionary");
+
+            AssertBencodeEqual(expected[key], actual[key]);
+        }
+    }
+
+    private void AssertBencodeEqual(object expected, object actual)
+    {
+        switch (expected)
+        {
+            case Dictionary<string, object> expectedDictionary:
+                var actualDictionary = Assert.IsType<Dictionary<string, object>>(actual);
+                AssertBencodeDictionaryEqual(expectedDictionary, actualDictionary);
+                break;
+
+            case List<object> expectedList:
+                var actualList = Assert.IsType<List<object>>(actual);
+                AssertBencodeListEqual(expectedList, actualList);
+                break;
+
+            case long expectedLong:
+                var actualLong = Assert.IsType<long>(actual);
+                Assert.Equal(expectedLong, actualLong);
+                break;
+
+            case string expectedString:
+                var actualString = Assert.IsType<string>(actual);
+                Assert.Equal(expectedString, actualString);
+                break;
+
+            default:
+                throw new ArgumentException($"Unsupported type: {expected.GetType()}");
+        }
+    }
+
+    public static IEnumerable<object[]> GetStringTestData()
+    {
+        yield return new object[] { "0:", "" };
+        yield return new object[] { "3:123", "123" };
+        yield return new object[] { "5:Wait!", "Wait!" };
+        yield return new object[] { "29:Til I come back to your side!", "Til I come back to your side!" };
+    }
+
+    public static IEnumerable<object[]> GetIntegerTestData()
+    {
+        yield return new object[] { "i0e", 0L };
+        yield return new object[] { "i42e", 42L };
+        yield return new object[] { "i-20e", -20L };
+        yield return new object[] { "i1234567890e", 1234567890L };
+    }
+
+    public static IEnumerable<object[]> GetListTestData()
+    {
+        yield return new object[] { "le", new List<object> { } };
+        yield return new object[] { "li100e2:aae", new List<object> { 100L, "aa" } };
+        yield return new object[] { "l0:3:1235:Wait!e", new List<object> { "", "123", "Wait!" } };
+        yield return new object[] { "li-20ei123ee", new List<object> { -20L, 123L } };
+        yield return new object[] {
+            "lleli123ei-123eel4:wait2:hiee",
+            new List<object> {
+                new List<object> { },
+                new List<object> { 123L, -123L },
+                new List<object> { "wait", "hi" }
+            }
+        };
+        yield return new object[] {
+            "ld3:foo3:bar3:baz3:quxe5:helloee",
+            new List<object> {
+                new Dictionary<string, object> {
+                    ["foo"] = "bar",
+                    ["baz"] = "qux"
+                },
+                "hello"
+            }
+        };
+    }
+
+    public static IEnumerable<object[]> GetDictionaryTestData()
+    {
+        yield return new object[] { "de", new Dictionary<string, object> { } };
+
+        yield return new object[] {
+            "d3:foo3:bare",
+            new Dictionary<string, object> {
+                ["foo"] = "bar"
+            }
+        };
+
+        yield return new object[] {
+            "d1:a1:x1:b1:y1:c1:ze",
+            new Dictionary<string, object> {
+                ["a"] = "x",
+                ["b"] = "y",
+                ["c"] = "z"
+            }
+        };
+
+        yield return new object[] {
+            "d6:numberi123e4:listl1:a1:be5:empty0:e",
+            new Dictionary<string, object> {
+                ["number"] = 123L,
+                ["list"] = new List<object> { "a", "b" },
+                ["empty"] = ""
+            }
+        };
+
+        yield return new object[] {
+            "d5:innerd3:foo3:bare9:outer_val5:helloee",
+            new Dictionary<string, object> {
+                    ["inner"] = new Dictionary<string, object> {
+                    ["foo"] = "bar"
+                },
+                ["outer_val"] = "hello"
+            }
+        };
+    }
+}
